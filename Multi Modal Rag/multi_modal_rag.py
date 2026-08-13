2844855b27c48e0a6e52a62249ada1736ff0f540
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-08-13 10:48:56 -0700

    update ai enhanced summary function

--- Multi Modal Rag/multi_modal_rag.py
+++ Multi Modal Rag/multi_modal_rag.py
@@ -128,6 +128,44 @@ def summarize_Chunks(chunks):
 
 def create_ai_enhanced_summary(text:str,tables:List[str],images:List[str])-> str:
     """create AI-enhanced summary for mixed content"""
+    try:
+        llm=ChatOllama(model="llama3.2-vision:11b", temperature=0.2)
+        prompt_text=f"""you are creating a searchable description for document content retrieval.
+        CONTENT TO ANALYZE:
+        TEXT CONTENT:
+        {text}"""
+
+        if tables:
+            prompt_text+="Tables:\n"
+            for i,table in enumerate(tables):
+                prompt_text+=f"Table {i+1}:\n {table}\n\n"
+
+                prompt_text+="""YOUR TASK:
+                Generate a comprehensive, searchable description that covers:
+
+                1. Key facts, numbers, and data points from text and tables
+                2. Main topics and concepts discussed
+                3. Questions this content could answer
+                4. Visual content analysis (charts, diagrams, patterns in images)
+                5. Alternative search terms users might use
+
+                Make it detailed and searchable - prioritize findability over brevity.
+
+                SEARCHABLE DESCRIPTION:"""
+
+            message_content = [{"type": "text", "text": prompt_text}]
+            for image_base64 in images:
+                    message_content.append({
+                "type": "image_url",
+                "image_url": {"url":f"data:image/jpeg;base64,{image_base64}"}
+            })
+            message = HumanMessage(content=message_content)
+            response = llm.invoke([message])
+
+            return response.content
+
+    except Exceptionas e:
+
 
 
 
